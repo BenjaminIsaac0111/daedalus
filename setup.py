c025a57e56c5ebe6a5600511058af8b309d46d3c
Benjamin Wilson <medbwila@leeds.ac.uk>
2020-03-04 10:51:37 +0000

    Work on setup.py and making sure travis is installing the correct packages before testing.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='daedalus',
+      version='0.0.1',
+      description='Microsimulation package for custom UK population projection',
+      long_description=readme(),
+      url='https://github.com/BenjaminIsaac0111/daedalus',
+      author='Benjamin Isaac Wilson',
+      author_email='medbwila@leeds.ac.uk',
+      license='MIT',
+      packages=['daedalus'],
+      zip_safe=False,
+      # NOTE: vivarium is not available via conda so needs to be installed via pip
+      install_requires=['distutils_pytest', 'humanleague', 'ukcensusapi', 'ukpopulation', 'vivarium'],
+      dependency_links=['git+git://github.com/BenjaminIsaac0111/humanleague.git#egg=humanleague',
+                        'git+git://github.com/BenjaminIsaac0111/UKCensusAPI.git#egg=ukcensusapi'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      python_requires='>=3'
+      )
