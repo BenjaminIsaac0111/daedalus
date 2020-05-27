2e461a9e35ac2eb26d3ca6a503f89fb3e67b1543
kasra-hosseini <khosseini@turing.ac.uk>
2020-05-27 16:36:19 +0100

    Minor changes

--- daedalus/mortality.py
+++ daedalus/mortality.py
@@ -23,19 +23,19 @@ class Mortality:
     # In an actual simulation, we’d inform the base mortality rate with data specific to the age, sex, location,
     # year (and potentially other demographic factors) that represent each simulant.
     def base_mortality_rate(self, index: pd.Index) -> pd.Series:
+        #df = self.population_view.get(index)
+        #gender_based_rate = []
+        #for i in index:
 
-        df = self.population_view.get(index)
-        gender_based_rate = []
-        for i in index:
+        #    print (i)
+        #    gender = df.iloc[i]['DC1117EW_C_SEX']
+        #    if gender == 1:
+        #        gender_based_rate.append(0.05)
+        #    else:
+        #        gender_based_rate.append(0.1)
 
-            print (i)
-            gender = df.iloc[i]['DC1117EW_C_SEX']
-            if gender == 1:
-                gender_based_rate.append(0.05)
-            else:
-                gender_based_rate.append(0.1)
-
-        return pd.Series(gender_based_rate, index=index)
+        #return pd.Series(gender_based_rate, index=index)
+        return pd.Series(self.config.mortality_rate, index=index)
 
     def determine_deaths(self, event: Event):
         effective_rate = self.mortality_rate(event.index)
@@ -45,7 +45,7 @@ class Mortality:
 
         self.population_view.update(pd.Series('dead', index=event.index[affected_simulants],name='alive'))
 
- #       population = self.population_view.get(event.index)
- #       population.loc[event.index[affected_simulants], 'alive'] = 'dead'
- #       self.population_view.update(population)
-        print(self.population_view.subview('alive') == 'dead')
+        #population = self.population_view.get(event.index)
+        #population.loc[event.index[affected_simulants], 'alive'] = 'dead'
+        #self.population_view.update(population)
+        #print(self.population_view.subview('alive') == 'dead')
