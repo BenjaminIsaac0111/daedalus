2d41fcea9c00b743fb47fe7402fb22afdfb9fdef
crangelsmith <camila.rangel.smith@gmail.com>
2020-10-16 12:47:35 +0100

    updating installation setup.py to use actual require file

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
+with open("requirements.txt", "r") as f:
+    REQUIRED_PACKAGES = f.read().splitlines()
 
 setup(name='daedalus',
       version='0.0.1',
@@ -18,15 +20,15 @@ setup(name='daedalus',
       license='MIT',
       packages=['daedalus'],
       zip_safe=False,
-
+      install_requires=REQUIRED_PACKAGES,
       # NOTE: vivarium is not available via conda so needs to be installed via pip
-      install_requires=['distutils_pytest', 'vivarium>=0.9.1',
-                        #'ukcensusapi @ git+https://github.com/BenjaminIsaac0111/UKCensusAPI.git@Development#egg=ukcensusapi',
-                        #'ukpopulation @ git+https://github.com/nismod/ukpopulation.git@Development#egg=ukpopulation',
-                        #'humanleague @ git+https://github.com/BenjaminIsaac0111/humanleague.git@Development#egg=humanleague',
-                        #'household_microsynth @ git+https://github.com/nismod/household_microsynth.git@Development#egg=household_microsynth',
-                        #'microsimulation @ git+https://github.com/nismod/microsimulation.git@Development#egg=microsimulation',
-                        'vivarium_public_health @ git+https://github.com/alan-turing-institute/vivarium_public_health_spenser.git@feature/51-assign-MSO-to-immigrants#egg=vivarium_public_health_spenser'],
+      # install_requires=['distutils_pytest', 'vivarium>=0.9.1',
+      #                   #'ukcensusapi @ git+https://github.com/BenjaminIsaac0111/UKCensusAPI.git@Development#egg=ukcensusapi',
+      #                   #'ukpopulation @ git+https://github.com/nismod/ukpopulation.git@Development#egg=ukpopulation',
+      #                   #'humanleague @ git+https://github.com/BenjaminIsaac0111/humanleague.git@Development#egg=humanleague',
+      #                   #'household_microsynth @ git+https://github.com/nismod/household_microsynth.git@Development#egg=household_microsynth',
+      #                   #'microsimulation @ git+https://github.com/nismod/microsimulation.git@Development#egg=microsimulation',
+      #                   'vivarium_public_health @ git+https://github.com/alan-turing-institute/vivarium_public_health_spenser.git@feature/51-assign-MSO-to-immigrants#egg=vivarium_public_health_spenser'],
 
       test_suite='nose.collector',
       tests_require=['nose'],
