7e46db24557e75479a6a52af25df3cf17b0ea773
crangelsmith <camila.rangel.smith@gmail.com>
2020-10-26 11:54:58 +0000

    uncommenting the broken test (#7)

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -21,19 +21,19 @@ def test_compare_summary_estimates():
 
     assert (summary_df.shape[0]==1)
 
-# def test_compare_detailed_estimates():
-# 
-#     simulation_data_file = os.path.join('tests','data','ssm_E08000032_MSOA11_ppp_2011_simulation.csv')
-# 
-#     ONS_data_file = os.path.join('persistent_data','MYEB2_detailed_components_of_change_series_EW_(2019_geog20).csv')
-# 
-#     simulation_data = pd.read_csv(simulation_data_file)
-#     ONS_data = pd.read_csv(ONS_data_file)
-# 
-#     summary_df = compare_detailed_estimates(simulation_data,ONS_data,'E08000032',1)
-#     summary_df.to_csv(os.path.join('tests','data','comparison_detailed_df.csv'))
-# 
-#     assert (summary_df.shape[0]>1)
+def test_compare_detailed_estimates():
+
+    simulation_data_file = os.path.join('tests','data','ssm_E08000032_MSOA11_ppp_2011_simulation.csv')
+
+    ONS_data_file = os.path.join('persistent_data','MYEB2_detailed_components_of_change_series_EW_(2019_geog20).csv')
+
+    simulation_data = pd.read_csv(simulation_data_file)
+    ONS_data = pd.read_csv(ONS_data_file)
+
+    summary_df = compare_detailed_estimates(simulation_data,ONS_data,'E08000032',1)
+    summary_df.to_csv(os.path.join('tests','data','comparison_detailed_df.csv'))
+
+    assert (summary_df.shape[0]>1)
 
 def test_run_pipeline():
 
