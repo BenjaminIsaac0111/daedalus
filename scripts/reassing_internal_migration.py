fa244546313164dc158e4c7de40fbeea01bcf584
crangelsmith <camila.rangel.smith@gmail.com>
2020-10-28 10:59:22 +0000

    refactoring the code for speed (#9)

--- scripts/reassing_internal_migration.py
+++ scripts/reassing_internal_migration.py
@@ -4,7 +4,7 @@ import pandas as pd
 import argparse
 
 
-def reassing_internal_migration_to_LAD(location, input_path, list_pop_locations_dir):
+def reassing_internal_migration_to_LAD(location, input_path, pool_migrants):
     """ Function that finds all the individuals that internally migrated into a given location
 
     Parameters
@@ -13,8 +13,8 @@ def reassing_internal_migration_to_LAD(location, input_path, list_pop_locations_
         LAD code for place to run the simulation
     input_path: str
         Input data path where the outputs of all simulations are found
-    list_pop_locations_dir:
-        list of other locations where to search for internal migration
+    pool_migrants: dict of Dataframe
+        Dictionary with pool of migrants from each year
 
     """
 
@@ -32,21 +32,17 @@ def reassing_internal_migration_to_LAD(location, input_path, list_pop_locations_
         simulation_data['duplicate'] = False
         simulation_data['internal_migration_in'] = 'No'
 
-        # look in each location finding the individuals that migrated
-        for loc in list_pop_locations_dir:
-            if loc==location:
-                continue
-            data_location_simulation = pd.read_csv(os.path.join(input_path, loc, year_dir,
-                                                                'ssm_' + loc + '_MSOA11_ppp_2011_simulation_' + year_dir + '.csv'))
 
-            data_location_simulation = data_location_simulation[data_location_simulation['location'] == location]
+        data_location_simulation = pool_migrants[year_dir]
 
-            if data_location_simulation.shape[0]>0:
-                print ('Reassign ',data_location_simulation.shape[0],' individuals that migrated from ',loc,' to ',location)
-                data_location_simulation['duplicate'] = True
-                data_location_simulation['internal_migration_in'] = 'Yes'
+        data_location_simulation = data_location_simulation[data_location_simulation['location'] == location]
 
-                simulation_data = pd.concat([simulation_data,data_location_simulation])
+        if data_location_simulation.shape[0]>0:
+            print ('Reassign ',data_location_simulation.shape[0],' individuals that migrated to ',location)
+            data_location_simulation['duplicate'] = True
+            data_location_simulation['internal_migration_in'] = 'Yes'
+
+            simulation_data = pd.concat([simulation_data,data_location_simulation])
 
         simulation_data.to_csv(os.path.join(input_location_path, year_dir,
                                             'ssm_' + location + '_MSOA11_ppp_2011_simulation_' + year_dir + '_reassigned.csv'))
@@ -54,25 +50,21 @@ def reassing_internal_migration_to_LAD(location, input_path, list_pop_locations_
     # run comparison for the total simmulation
     final_file = 'ssm_' + location + '_MSOA11_ppp_2011_simulation.csv'
 
-    simulation_data_full = pd.read_csv(os.path.join(input_location_path, final_file),low_memory=False)
+    simulation_data_full = pd.read_csv(os.path.join(input_location_path,
+                                                   'ssm_' + location + '_MSOA11_ppp_2011_simulation.csv'))
 
-    print('Reassign full dataset ')
-    for loc in list_pop_locations_dir:
-        if loc == location:
-            continue
-        data_location_simulation_full = pd.read_csv(os.path.join(input_path, loc,
-                                                            'ssm_' + loc + '_MSOA11_ppp_2011_simulation.csv'))
+    data_location_simulation_full = pool_migrants['full']
 
-        data_location_simulation_full = data_location_simulation_full[data_location_simulation_full['location'] == location]
+    data_location_simulation_full = data_location_simulation_full[data_location_simulation_full['location'] == location]
 
-        if data_location_simulation_full.shape[0] > 0:
+    if data_location_simulation_full.shape[0] > 0:
+        print ('Full dataset')
+        print('Reassign ', data_location_simulation_full.shape[0], ' individuals that migrated from  to ', location)
 
-            print('Reassign ', data_location_simulation_full.shape[0], ' individuals that migrated from ', loc, ' to ', location)
+        data_location_simulation_full['duplicate'] = True
+        data_location_simulation_full['internal_migration_in'] = 'Yes'
 
-            data_location_simulation_full['duplicate'] = True
-            data_location_simulation_full['internal_migration_in'] = 'Yes'
-
-            simulation_data_full = pd.concat([simulation_data_full, data_location_simulation_full])
+        simulation_data_full = pd.concat([simulation_data_full, data_location_simulation_full])
 
     simulation_data_full.to_csv(os.path.join(input_location_path,
                                         final_file+'_reassigned.csv'))
@@ -82,16 +74,88 @@ def reassing_all(input_data_dir):
     """
     Run reassigment in all existing locations present in an input directory
 
+    Parameters:
+    ----------
     input_data_dir : string
         Input data path where the outputs of all simulations are found
+    Returns:
+    ----------
+        A dictionary of dataframes with the pool of migrants for each year.
     """
 
     list_pop_locations_dir = [i for i in os.listdir(input_data_dir) if i.startswith('E')]
 
+    pool_migrants = get_migrants(input_data_dir, list_pop_locations_dir)
+
     for location in list_pop_locations_dir:
         print ()
         print('Running re-assigment for: ', location)
-        reassing_internal_migration_to_LAD(location, input_data_dir, list_pop_locations_dir)
+        reassing_internal_migration_to_LAD(location, input_data_dir, pool_migrants)
+
+
+def get_migrants(input_path, list_pop_locations_dir):
+    """ Function that finds all the individuals that internally migrated into a given location
+
+    Parameters
+    ----------
+    location: str
+        LAD code for place to run the simulation
+    input_path: str
+        Input data path where the outputs of all simulations are found
+    list_pop_locations_dir:
+        list of other locations where to search for internal migration
+
+    """
+
+    input_location_path = os.path.join(input_path, list_pop_locations_dir[0])
+    # path to the simulation files
+    list_pop_dir = [i for i in os.listdir(input_location_path) if i.startswith('year_')]
+
+    dict_migrants = {}
+    # for each year run the reassigment
+    for year_dir in list_pop_dir:
+        print ('Get migrants for ', year_dir)
+
+        migrant_data = pd.DataFrame()
+        # look in each location finding the individuals that migrated
+
+        if year_dir == list_pop_dir[-1]:
+            full_migrant_data= pd.DataFrame()
+
+        for loc in list_pop_locations_dir:
+
+            data_location_simulation = pd.read_csv(os.path.join(input_path, loc, year_dir,
+                                                                'ssm_' + loc + '_MSOA11_ppp_2011_simulation_' + year_dir + '.csv'))
+
+            data_location_simulation_int_migrants = data_location_simulation[data_location_simulation['location'] != loc]
+
+            if data_location_simulation_int_migrants.shape[0]>0:
+                print ('Found ',data_location_simulation.shape[0],' individuals that migrated' )
+                data_location_simulation_int_migrants['duplicate'] = True
+                data_location_simulation_int_migrants['internal_migration_in'] = 'Yes'
+
+                migrant_data = pd.concat([migrant_data,data_location_simulation_int_migrants])
+
+            if year_dir == list_pop_dir[-1]:
+                data_location_simulation_full = pd.read_csv(os.path.join(input_path, loc,
+                                                                         'ssm_' + loc + '_MSOA11_ppp_2011_simulation.csv'))
+
+                data_location_simulation_full_migrants = data_location_simulation_full[data_location_simulation_full['location'] != loc]
+                if data_location_simulation_full_migrants.shape[0] > 0:
+                    print('Found ', data_location_simulation_full_migrants.shape[0], ' individuals that migrated')
+                    data_location_simulation_full_migrants['duplicate'] = True
+                    data_location_simulation_full_migrants['internal_migration_in'] = 'Yes'
+
+                    full_migrant_data = pd.concat([full_migrant_data, data_location_simulation_full_migrants])
+
+        dict_migrants[year_dir] = migrant_data
+
+        if year_dir == list_pop_dir[-1]:
+            dict_migrants['full'] = full_migrant_data
+
+    # run comparison for the total simmulation
+    return dict_migrants
+
 
 
 if __name__ == "__main__":
@@ -104,6 +168,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.location:
         list_pop_locations_dir = [i for i in os.listdir(args.input_data_dir) if i.startswith('E')]
-        reassing_internal_migration_to_LAD(args.location, args.input_data_dir, list_pop_locations_dir)
+        pool_migrants = get_migrants(args.input_data_dir, list_pop_locations_dir)
+        reassing_internal_migration_to_LAD(args.location, args.input_data_dir, pool_migrants)
     else:
         reassing_all(args.input_data_dir)
