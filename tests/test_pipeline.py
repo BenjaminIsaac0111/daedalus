a98c707f07a8dce2a8771d152a8c19149dcc04bd
crangelsmith <camila.rangel.smith@gmail.com>
2020-10-29 10:29:41 +0000

    fixing broken CI (#8)

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -8,14 +8,14 @@ import os
 
 def test_compare_summary_estimates():
 
-    simulation_data_file = os.path.join('tests','data','ssm_E08000033_MSOA11_ppp_2011_simulation.csv')
+    simulation_data_file = os.path.join('tests','data','ssm_E08000032_MSOA11_ppp_2011_simulation_reassigned.csv')
 
     ONS_data_file = os.path.join('persistent_data','MYEB3_summary_components_of_change_series_UK_(2019_geog20).csv')
 
     simulation_data = pd.read_csv(simulation_data_file)
     ONS_data = pd.read_csv(ONS_data_file)
 
-    summary_df = compare_estimates(simulation_data, ONS_data, "E08000033", 3)
+    summary_df = compare_estimates(simulation_data, ONS_data, "E08000033", 2)
 
     summary_df.to_csv(os.path.join('tests','data','comparison_summary_df.csv'))
 
@@ -23,7 +23,7 @@ def test_compare_summary_estimates():
 
 def test_compare_detailed_estimates():
 
-    simulation_data_file = os.path.join('tests','data','ssm_E08000032_MSOA11_ppp_2011_simulation.csv')
+    simulation_data_file = os.path.join('tests','data','ssm_E08000032_MSOA11_ppp_2011_simulation_reassigned.csv')
 
     ONS_data_file = os.path.join('persistent_data','MYEB2_detailed_components_of_change_series_EW_(2019_geog20).csv')
 
